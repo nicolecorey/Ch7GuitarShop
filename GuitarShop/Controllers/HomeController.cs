fe8a8ec8adaa6e47afc8bac1e7ac54a5687a0496
Nicole <wolfn@mail.nmc.edu>
2023-10-23 17:25:07 -0400

    Add project files.

---
+++ GuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("[action]")]
+
+        public IActionResult ContactUs()
+        {
+            var contact = new Dictionary<string, string>
+            {
+                {"Phone:", "555-555-5555" },
+                {"Email:", "info@myguitarshop.com"}
+            };
+            return View(contact);
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
